2156170b5be8a513053bd327b436486ff7ed0314
VadimKorobec <_s_e_t__@ukr.net>
2025-06-17 15:24:38 +0300

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
-import { useEffect, useState } from "react";
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [liked, setLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${liked}`);
-  }, [liked, title]);
-
-  useEffect(() => {
-    console.log("CARD RENDERED");
-  }, []);
-
-  const handleChangeLike = () => {
-    setLiked((prevState) => !prevState);
-  };
-
-  const handleIncreaseCount = () => {
-    setCount((prevState) => prevState + 1);
-  };
-
-  return (
-    <div className="card" onClick={handleIncreaseCount}>
-      <h2>
-        {title} <br /> {count}
-      </h2>
-      <button onClick={handleChangeLike}>{liked ? "🩷" : "🤍"}</button>
-    </div>
-  );
-};
-
 const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Start Wars" />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
