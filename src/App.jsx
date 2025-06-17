13f2b29cb0c029fcecce25b917531369475383ed
VadimKorobec <_s_e_t__@ukr.net>
2025-06-17 17:55:42 +0300

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Search from "./components/Search";
 
+const API_BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY} `,
+  },
+};
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  console.log(searchTerm);
+  const [movies, setMovies] = useState(null);
+  console.log(movies);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(API_BASE_URL, API_OPTIONS);
+        if (!response.ok) {
+          return;
+        }
+        const data = await response.json();
+        console.log(data);
+        setMovies(data.results);
+      } catch (error) {
+        console.error(`Error fetching movies:${error} `);
+      }
+    };
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -20,6 +49,17 @@ const App = () => {
         <Search value={searchTerm} onSearch={setSearchTerm} />
         <h1>{searchTerm}</h1>
       </div>
+      {movies ? (
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <p className="text-white">{movie.original_title}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-white">Loading...</p>
+      )}
     </main>
   );
 };
