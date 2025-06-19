5a746f06e8cd1fe70557ce4432f6fb5d304d963c
VadimKorobec <_s_e_t__@ukr.net>
2025-06-19 16:51:27 +0300

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -41,6 +42,7 @@ const App = () => {
         }
         const data = await response.json();
         setMovieList(data.results);
+        updateSearchCount();
       } catch (error) {
         console.error(`Error fetching movies: ${error}`);
         setErrorMessage("Error fetching movies. Please try again later.");
